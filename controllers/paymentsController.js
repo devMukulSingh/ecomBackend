47604be91e84f9ed0a1b8fdb46204a3f3fdb0760
JOHN CARTER <mukulsingh2276@gmail.com>
2023-10-28 11:20:18 +0530

    paytm removes

--- controllers/paymentsController.js
+++ controllers/paymentsController.js
@@ -1,65 +1,65 @@
-import { paytmMerchantKey, paytmParams } from "..";
-import PaytmChecksum from "../paytm/PaytmChecksum.js";
-import https from "https";
-import formidable from "formidable";
+// import { paytmMerchantKey, paytmParams } from "..";
+// import PaytmChecksum from "../paytm/PaytmChecksum.js";
+// import https from "https";
+// import formidable from "formidable";
 
 
-export const paymentController = async(req,res) => {
+// export const paymentController = async(req,res) => {
     
-    try {
-        const paytmChecksum = await PaytmChecksum.generateSignature(paytmParams, paytmMerchantKey);
+//     try {
+//         const paytmChecksum = await PaytmChecksum.generateSignature(paytmParams, paytmMerchantKey);
     
-        const params = {
-            ...paytmParams, 'CHECKSUMHASH':paytmChecksum
-        }
-        res.status(200).json(params);
-    } catch (error) {
-        res.status(500).json(`Error in paymentController ${error}`);
-    }
-} 
-export const paymentResponse = async(req,res) => {
-    const form = new formidable.IncomingForm();
-    const paytmChecksum = req.body.CHECKSUMHASH;
-    delete req.body.CHECKSUMHASH;
+//         const params = {
+//             ...paytmParams, 'CHECKSUMHASH':paytmChecksum
+//         }
+//         res.status(200).json(params);
+//     } catch (error) {
+//         res.status(500).json(`Error in paymentController ${error}`);
+//     }
+// } 
+// export const paymentResponse = async(req,res) => {
+//     const form = new formidable.IncomingForm();
+//     const paytmChecksum = req.body.CHECKSUMHASH;
+//     delete req.body.CHECKSUMHASH;
 
-    const isVerifySignature = paytmChecksum.verifySignature(req.body.paytmMerchantKey, paytmChecksum);
-    if(isVerifySignature){
-        const paytmParams = {};
-        paytmParams['MID'] = req.body.MID;
-        paytmParams['ORDERID'] = req.body.ORDERID;
+//     const isVerifySignature = paytmChecksum.verifySignature(req.body.paytmMerchantKey, paytmChecksum);
+//     if(isVerifySignature){
+//         const paytmParams = {};
+//         paytmParams['MID'] = req.body.MID;
+//         paytmParams['ORDERID'] = req.body.ORDERID;
 
-        paytmChecksum.generateSignature(paytmParams, paytmMerchantKey).then( function(checksum){
+//         paytmChecksum.generateSignature(paytmParams, paytmMerchantKey).then( function(checksum){
 
-            paytmParams['CHECKSUM'] = checksum;
-            const post_data = JSON.stringify(paytmParams);
+//             paytmParams['CHECKSUM'] = checksum;
+//             const post_data = JSON.stringify(paytmParams);
 
-            const options = {
-                hostname :' securegw-stage.paytm.in',
-                port: 443,
-                path: '/order/status',
-                headers : {
-                    'Content-Type': 'application/json',
-                    'Content-Length' : post_data.length
-                }
-            }
-            let res = "";
-            const post_req = https.request(options, function( post_res ) {
-                post_res.on('data',function(chunk){
-                    res+=chunk;
+//             const options = {
+//                 hostname :' securegw-stage.paytm.in',
+//                 port: 443,
+//                 path: '/order/status',
+//                 headers : {
+//                     'Content-Type': 'application/json',
+//                     'Content-Length' : post_data.length
+//                 }
+//             }
+//             let res = "";
+//             const post_req = https.request(options, function( post_res ) {
+//                 post_res.on('data',function(chunk){
+//                     res+=chunk;
 
-                })
-                post_res.on('end', function() {
-                    const result = JSON.parse(res)
-                    response.redirect('http://localhost:3000/')
-                })
-            });
-            post_req.write(post_data);
-            post_req.end();
-        })
+//                 })
+//                 post_res.on('end', function() {
+//                     const result = JSON.parse(res)
+//                     response.redirect('http://localhost:3000/')
+//                 })
+//             });
+//             post_req.write(post_data);
+//             post_req.end();
+//         })
         
         
-    }
-    else{
-        console.log(`Checksum Mismatched`);
-    }
-}
+//     }
+//     else{
+//         console.log(`Checksum Mismatched`);
+//     }
+// }
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import router from "./routes/routes.js";
 import db from "./db/db.js";
 import defaultData from "./default.js";
 const app = express();
-import { v4 as uuid} from "uuid";
+// import { v4 as uuid} from "uuid";
 
 const PORT = process.env.PORT || 8000;
 
@@ -18,18 +18,18 @@ app.listen( PORT , () => {
 })
 defaultData();
 
-export const paytmMerchantKey = process.env.PAYTM_MERCHANT_KEY;
-export const paytmParams = {};
-paytmParams['MID'] = process.env.PAYTM_MID;
-paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE;
-paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID;
-paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID;
-paytmParams['ORDER_ID'] = uuid();
-paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID;
-paytmParams['TXN_AMOUNT'] = 100;
-paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback';
-paytmParams['EMAIL'] = 'mukulsingh2276@gmail.com';
-paytmParams['MOBILE_NO'] = '1234567890';
+// export const paytmMerchantKey = process.env.PAYTM_MERCHANT_KEY;
+// export const paytmParams = {};
+// paytmParams['MID'] = process.env.PAYTM_MID;
+// paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE;
+// paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID;
+// paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID;
+// paytmParams['ORDER_ID'] = uuid();
+// paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID;
+// paytmParams['TXN_AMOUNT'] = 100;
+// paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback';
+// paytmParams['EMAIL'] = 'mukulsingh2276@gmail.com';
+// paytmParams['MOBILE_NO'] = '1234567890';
 
 
 
